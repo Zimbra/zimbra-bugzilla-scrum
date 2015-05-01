224baa7818ddd0e830435a47ce048f1b23330bf9
David Rauschenbach <davidr@zimbra.com>
2015-05-01 15:45:26 -0700

    Added /products route, JSONP adapter, model, and template. Not useful, but loads and displays the data, to create the patterns to copy from.

---
+++ app/adapters/product.js
@@ -0,0 +1,48 @@
+import ApplicationAdapter from './application';
+import Ember from 'ember';
+
+
+//
+// findAll 
+//
+var findAll = function (store, type) {
+  var promise = new Ember.RSVP.Promise(function(resolve, reject) {
+    Ember.$.ajax({
+      url: 'https://bugzilla.zimbra.com/jsonrpc.cgi?method=Product.get_accessible_products',
+      dataType: 'jsonp',
+      context: store,
+      error: function(xhr, ajaxOptions, thrownError) {
+        reject(thrownError);
+      },
+      success: function(response) {
+        if (response.error) {
+          reject(response.error);
+          return;
+        }
+        
+        var url = 'https://bugzilla.zimbra.com/jsonrpc.cgi?method=Product.get&params=[{"ids":[' + response.result.ids + ']}]';
+        Ember.$.ajax({
+          url: url,
+          dataType: 'jsonp',
+          context: store,
+          error: function(xhr, ajaxOptions, thrownError) {
+            reject(thrownError);
+          },
+          success: function(response) {
+            if (response.error) {
+              reject(response.error);
+              return;
+            }
+            resolve({products:response.result.products});
+          }
+        });
+        
+      }
+    });
+  });
+  return promise;
+};
+
+export default ApplicationAdapter.extend({
+  findAll: findAll
+});
