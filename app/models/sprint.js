3ce2b4621b0c477eea52a0b938f4e2715c162ff8
David Rauschenbach <davidr@zimbra.com>
2015-05-02 10:39:20 -0700

    Sprint model now encapsulates begin and end dates, and status of whether it was started, ended, or is current.

--- app/models/sprint.js
+++ app/models/sprint.js
@@ -1,5 +1,23 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr('string')
+  begin: DS.attr('date'),
+  end: DS.attr('date'),
+  
+  name: function() {
+    return 'S' + this.get('id');
+  }.property('id'),
+  
+  hasStarted: function() {
+    return moment(this.get('begin')).isBefore(moment());
+  }.property('begin'),
+  
+  hasEnded: function() {
+    return moment(this.get('end')).isBefore(moment());
+  }.property('end'),
+  
+  isCurrent: function() {
+    return this.get('hasStarted') && !this.get('hasEnded');
+  }.property(['hasStarted', 'hasEnded'])
+  
 });
