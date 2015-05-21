8f69d1daa56d6c85b2233c3dce7807d9f7858498
David Rauschenbach <davidr@zimbra.com>
2015-05-21 10:14:13 -0700

    Fixes #14.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,8 +1,3 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
-  currentPathDidChange: function() {
-    var currentPath = this.get('currentPath');
-    ZimbraBugzillaScrum.set('currentPath', currentPath);
-  }.observes('currentPath')
-});
+export default Ember.Controller.extend({});
