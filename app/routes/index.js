ca2857aaf54831907bb2783f14797a7b43ebbcd8
David Rauschenbach <davidr@zimbra.com>
2015-07-14 11:27:29 -0700

    Fixes #4. Show epics in progress on landing page. Rework layout with Bootstrap for adaptive 2-column on wide screens, or 1-column on small. Filtering on epics is done by Summary field for speed, since server-side keyword filters are unavailable ATM.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  
   model: function() {
     var self = this;
     return self.store.find('sprint', {isCurrent:true}).then(function(array) {
       return array.get('firstObject');
     });
+  },
+  
+  setupController: function(controller, model) {
+    this._super(controller, model);
+    controller.set('epicsInProgress', this.store.find('bug', {summary:'(Epic)', status:'IN_PROGRESS'}));
   }
+  
 });
