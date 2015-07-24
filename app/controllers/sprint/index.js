c552123941aea6899e879b310792720f0b3bb1b2
David Rauschenbach <davidr@zimbra.com>
2015-07-24 12:55:46 -0700

    Fixes #27. Sort completed bugs by status equals CLOSED, VERIFIED, then RESOLVED.

--- app/controllers/sprint/index.js
+++ app/controllers/sprint/index.js
@@ -2,12 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  doneBugs: function() {
+  doneBugs_: function() {
     var bugs = this.get('model.bugs');
     if (bugs) {
       return bugs.filterBy('isDone', true);
     }
   }.property('model.bugs.length'),
+  
+  doneBugs: Ember.computed.sort('doneBugs_', function(a, b) {
+    var sort = ['CLOSED', 'VERIFIED', 'RESOLVED'];
+    return sort.indexOf(Ember.get(a, 'status')) - sort.indexOf(Ember.get(b, 'status')); 
+  }),
   
   toDoBugs: function() {
     var bugs = this.get('model.bugs');
